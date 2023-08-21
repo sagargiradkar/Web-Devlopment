549555ac13c6bae892673706aada6df174402002
sagargiradkar <spgiradkar2002@gmail.com>
2023-08-22 01:49:46 +0530

    Ecomarce

---
+++ Projects/Ecome/app.js
@@ -0,0 +1,43 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "images\air1.jpg",
+      },
+     
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+     
+    ],
+  },
+  
+
+]

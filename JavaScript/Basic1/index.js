0e4198f606cb6eaab27768dfc7819890b85b3f24
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-09-12 10:28:27 +0530

    hhhh

---
+++ JavaScript/Basic1/index.js
@@ -0,0 +1,11 @@
+ // line below is used to print on the console window
+ console.log("Namaste Dunia version3");
+ console.log("Hello");
+
+ let a = 5 
+ console.log(a)
+
+ let srint = 'sagar'
+ console.log(srint)
+ 
+ const num = 12

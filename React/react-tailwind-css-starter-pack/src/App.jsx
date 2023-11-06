f0dff0f8f69aa9c488da737324bcb1b6748029a7
Sagar Giradkar <129714089+Smit-budgetalizer@users.noreply.github.com>
2023-11-06 18:18:28 +0530

    First React Project

--- React/react-tailwind-css-starter-pack/src/App.jsx
+++ React/react-tailwind-css-starter-pack/src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count,setCount] = useState(0);
+  function decreseHandler(){
+    setCount(count-1);
+  }
+  function increaseHandler(){
+    setCount(count+1)
+  }
+  function resetHandler(){
+    setCount(0)
+  }
   return (
-    <main className="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 className="bg-pink-400">Hello World</h1>
-      </main>
+
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div className="text-[#0398d4] font-medium text-2xl">Increment && Decrement</div>
+      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+        <button onClick={decreseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          -
+        </button>
+      <div className="font-bold gap-12 text-5xl">
+        {count}
+      </div>
+        <button onClick={increaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          +
+        </button>
+      </div>
+      <button onClick={resetHandler} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg">
+      Reset
+      </button>
+    </div>
   );
 }
 
